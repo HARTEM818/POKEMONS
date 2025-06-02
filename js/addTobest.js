1ebe50cfd16d558251b0a6c554a2b93c46aad1ca
HARTEM818 <artemhavryliuk777@gmail.com>
2025-06-02 10:32:45 +0300

    upf]d

--- js/addTobest.js
+++ js/addTobest.js
@@ -1,23 +1,40 @@
 import { refs } from "./refs.js";
 import { getPokemonInfo } from "./getPokemon.js";
 
+
 export async function addToBest() {
   const data = await getPokemonInfo(refs.inputForSearchPokemons.value);
+
   refs.addBestBtn.addEventListener("click", () => {
-    const p = document.createElement("p");
-    const deleteBtn = document.createElement("button");
-    deleteBtn.innerHTML = "delete";
-    p.innerHTML = data.name;
-    localStorage.setItem("best", data.name);
+    const favorites = JSON.parse(localStorage.getItem("favoritePokemons")) || [];
 
-    refs.bestPokemons.appendChild(p);
-    refs.bestPokemons.appendChild(deleteBtn);
+    favorites.push(data.name);
+    localStorage.setItem("favoritePokemons", JSON.stringify(favorites));
 
-    deleteBtn.addEventListener("click", () => {
-      p.style.display = "none";
-      deleteBtn.style.display = "none";
-      refs.addBestBtn.style.display = "block";
-    });
+    renderFavorite(data.name);
     refs.addBestBtn.style.display = "none";
+  });
+}
+
+
+export function renderFavorite(name) {
+  const p = document.createElement("p");
+  p.innerText = name;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "delete";
+
+  refs.bestPokemons.appendChild(p);
+  refs.bestPokemons.appendChild(deleteBtn);
+
+  deleteBtn.addEventListener("click", () => {
+    p.remove();
+    deleteBtn.remove();
+
+    const favorites = JSON.parse(localStorage.getItem("favoritePokemons")) || [];
+    const updated = favorites.filter((item) => item !== name);
+    localStorage.setItem("favoritePokemons", JSON.stringify(updated));
+
+    refs.addBestBtn.style.display = "block";
   });
 }
--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 import { getPokemonInfo } from "./getPokemon.js";
 import { refs } from "./refs.js";
 import { preparationInfo } from "./preparationInfo.js";
-import { addToBest } from "./addTobest.js";
-refs.searchBtn.addEventListener("click", async () => {
+import { addToBest, renderFavorite } from "./addTobest.js";
 
+refs.searchBtn.addEventListener("click", async () => {
   try {
     const prepared = await preparationInfo();
 
     const infoText = document.querySelector(".info-text");
-    infoText.innerHTML = ""; 
+    infoText.innerHTML = "";
 
     prepared.forEach((elem) => {
       infoText.appendChild(elem);
@@ -19,7 +19,14 @@ refs.searchBtn.addEventListener("click", async () => {
 
     addToBest();
   } catch (error) {
-    console.error("fuck");
-    
+    console.error("помилка");
   }
+});
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  const favorites = JSON.parse(localStorage.getItem("favoritePokemons")) || [];
+  favorites.forEach((name) => {
+    renderFavorite(name);
+  });
 });
